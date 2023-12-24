f2a2306d66945c3eba8ecb3a8d200adb890d96fc
xbvkano <kanomarcos1974@gmail.com>
2023-12-24 02:13:51 -0800

    Naming convention UpperCamelStyle

---
+++ src/components/Business.js
@@ -0,0 +1,33 @@
+import React from 'react';
+
+
+const business = {
+    imageSrc: 'https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg',
+    name: 'MarginOtto Pizzeria',
+    address: '1010 Paddington Way',
+    city: 'Flavortown',
+    state: 'NY',
+    zipCode: '10101',
+    category: 'Italian, Pizza',
+    rating: 4.5,
+    reviewCount: 90
+};
+
+function Business() {
+    return (
+        <div className="Business">
+            <img src={business.image}/>
+            <h2>{business.name}</h2>
+            <div className="Business-information">
+                <p className="address">{business.address}</p>
+                <p className="city">{business.city}</p>
+                <p className="state">{business.state} {business.zipCode}</p>
+                <p className="category">{business.category}</p>
+                <p className="rating">{business.rating} stars</p>
+                <p className="review-count">{business.reviewCount} reviews</p>
+            </div>
+        </div>
+    );
+}
+
+export default Business;
---
+++ src/components/BusinessList.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Business from './Business';
+
+function BusinessList() {
+    return (
+        <div className="BusinessList">
+            <Business />
+            <Business />
+            <Business />
+            <Business />
+            <Business />
+            <Business />
+        </div>
+    );
+}
+
+export default BusinessList;
